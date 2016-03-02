37f1ee0640da4042dd756608060ada529d39b2e5
Alisha <amauck@radiantblue.com>
2016-03-02 15:54:16 -0500

    story 1435

---
+++ scripts/black-box-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# Gather some data about the repo
+source $base/vars.sh
+
+#Run the test
+newman -sc $base/tests/PiazzaDevelopment.json.postman_collection
+newman -sc $base/tests/pz-register-test.json
+newman -sc $base/tests/testServiceControllerRestServicesSeq.json.postman_collection
+newman -sc $base/tests/UUID_Logger.json.postman_collection
