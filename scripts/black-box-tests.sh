0b5ce81c8e027c99fc7005b469bc9d5a811e47b0
Alisha <amauck@radiantblue.com>
2016-03-23 13:20:06 -0400

    fixshell

--- scripts/black-box-tests.sh
+++ scripts/black-box-tests.sh
@@ -8,14 +8,9 @@ popd > /dev/null
 source $base/vars.sh
 
 #Run the test
-<<<<<<< HEAD
-newman -sc $base/tests/PiazzaDevelopment.json.postman_collection
-newman -sc $base/tests/pz-register-test.json
-newman -sc $base/tests/testServiceControllerRestServicesSeq.json.postman_collection
-newman -sc $base/tests/UUID_Logger.json.postman_collection
-=======
+
 newman -sc $base/system-tests/PiazzaDevelopment.json.postman_collection
 newman -sc $base/system-tests/pz-register-test.json
 newman -sc $base/system-tests/testServiceControllerRestServicesSeq.json.postman_collection
 newman -sc $base/system-tests/UUID_Logger.json.postman_collection
->>>>>>> df1e73f47a1bf9ae82fd698895d1ba65ae661b68
+
