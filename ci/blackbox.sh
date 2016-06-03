8c9d4f0ac4a7685baa72ecdc0ce6cd2ebf6aaf98
amauck <amauck@radiantblue.com>
2016-06-03 11:15:31 -0400

    intbash

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -25,5 +25,28 @@ for f in $(ls -1 $base/postman/*postman_collection); do
 		mail -s "$SUBJ" $RCVR < /dev/null  
 	fi
    #awm	
+
+
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+[ -z "$space" ] && space=int
+
+envfile=$base/environments/$space.postman_environment
+
+[ -f $envfile ] || { echo "no tests configured for this environment"; exit 0; }
+
+cmd="newman -xe $envfile -c"
+
+for f in $(ls -1 $base/postman/*postman_collection); do
+  $cmd $f
+  #send mail if return value of last command is not 0
+	if [ "$?" -ne "0" ];then
+		mail -s "$SUBJ" $RCVR < /dev/null  
+	fi
+   #awm	
+
+
 done
 
