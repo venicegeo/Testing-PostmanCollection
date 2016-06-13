375cde356e59ab6b6420cadbd41ef9ef7a2fdd48
amauck <amauck@radiantblue.com>
2016-06-13 14:44:45 -0400

    revertstage

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -10,7 +10,7 @@ pushd `dirname $0` > /dev/null
 base=$(pwd -P)
 popd > /dev/null
 
-[ -z "$space" ] && space=int
+[ -z "$space" ] && space=stage
 
 envfile=$base/environments/$space.postman_environment
 
