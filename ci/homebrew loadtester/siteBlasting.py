5977293e960f27e725389fa83c0a591ba21033e9
Yarrington <jyarrington@radiantblue.local>
2016-07-11 10:42:26 -0400

    log in csv

--- ci/homebrew loadtester/siteBlasting.py
+++ ci/homebrew loadtester/siteBlasting.py
@@ -2,16 +2,24 @@ import multiprocessing
 import time
 import requests
 import sys
+import csv
 
 # Function to pass to new process.  Records elapsed time to the processes Array.  If a bad
 # status is returned, it is recorded in the status variable to be processed by the logger.
 def sendGetRequest(url, startQueue, resultQueue):
 	startQueue.put(1)
-	r = requests.get(url)
-	startQueue.get()
-	finishTime = time.time()
-	active = startQueue.qsize()
-	resultQueue.put((finishTime, r.status_code, r.reason, r.elapsed.total_seconds(), active)) # (time when complete, status code, reason for failure, request time, queue size)
+	try:
+		r = requests.get(url)
+	except Exception as e:
+		startQueue.get()
+		finishTime = time.time()
+		active = startQueue.qsize()
+		resultQueue.put((finishTime, 0, str(e), 0, active))
+	else:
+		startQueue.get()
+		finishTime = time.time()
+		active = startQueue.qsize()
+		resultQueue.put((finishTime, r.status_code, r.reason, r.elapsed.total_seconds(), active)) # (time when complete, status code, reason for failure, request time, queue size)
 
 # Logs imformation about running processes.  Handles request errors as they happen.
 def logger(num, startQueue, resultQueue):
@@ -26,7 +34,16 @@ def logger(num, startQueue, resultQueue):
 		if percentage >= next_percentage:
 			print('%3.0f%%' % percentage)
 			next_percentage += 5
-	print(results)
+	print('Blasting Complete!')
+	newResults = [('Time Completed', 'Status Code', 'Status Message', 'Response Time', 'Active Requests')]
+	for result in results:
+		newResults += [(result[0] - startTime, result[1], result[2], result[3], result[4])]
+	with open('test.csv', 'w') as test_file:
+		file_writer = csv.writer(test_file, lineterminator = '\n')
+		for line in newResults:
+			file_writer.writerow(line)
+	print('Log Written!')
+
 
 # Returns a list of processes that call sendGetRequest().
 def createProcesses(url, num, startQueue, resultQueue):
