ad6fc5a287556a642b9e48185c3332bd109b9079
James Yarrington <jyarrington@vt.edu>
2016-11-30 12:34:55 -0500

    use -o for v2 cl options

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -31,7 +31,7 @@ for space in $spaces; do
 
 	[ -f $envfile ] || { echo "no tests configured for this environment"; exit 0; }
 
-	cmd="newman --reporters cli,json --reporter-json-export results.json --requestTimeout 120000 -x -e $envfile -c"
+	cmd="newman -o results.json --requestTimeout 120000 -x -e $envfile -c"
 
 	latch=0
 
