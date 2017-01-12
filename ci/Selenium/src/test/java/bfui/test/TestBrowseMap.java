9fbdb83e759f2812ecd7089bedf88e8c73569d2c
James Yarrington <jyarrington@vt.edu>
2017-01-12 15:07:29 -0500

    more nav tests

--- ci/Selenium/src/test/java/bfui/test/TestBrowseMap.java
+++ ci/Selenium/src/test/java/bfui/test/TestBrowseMap.java
@@ -3,6 +3,8 @@ package bfui.test;
 import static org.junit.Assert.*;
 
 import java.awt.Robot;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.*;
 import org.openqa.selenium.By;
@@ -27,8 +29,19 @@ public class TestBrowseMap {
 	private String driverPath = System.getenv("driver_path");
 	private String browserPath = System.getenv("browser_path");
 	
-	private static String SriLankaCoords = "7.5, 80";
+	private static double SriLankaLat = 7.5;
+	private static double SriLankaLon = 80;
+	private static double PosDateLineLat = 20;
+	private static double PosDateLineLon = 180;
+	private static double NegDateLineLat = 20;
+	private static double NegDateLineLon = -180;
+	private static double NorthPoleLat = 90;
+	private static double NorthPoleLon = 5;
+	private static double SouthPoleLat = -90;
+	private static double SouthPoleLon = 5;
 	
+	private double lat;
+	private double lon;
 	
 	// Elements used:
 	private WebElement searchButton;
@@ -78,16 +91,96 @@ public class TestBrowseMap {
 		coordField = coordWindow.findElement(By.cssSelector("input[placeholder='Enter Coordinates']"));
 		submitButton = coordWindow.findElement(By.cssSelector("button[type=submit]"));
 		
-		coordField.sendKeys(SriLankaCoords);
+		// Check Normal Location
+		coordField.sendKeys(Double.toString(SriLankaLat) + ", " + Double.toString(SriLankaLon));
+		Thread.sleep(1000);
 		submitButton.click();
 		
 		robot.mouseMove(350, 350);
-		System.out.println("Current Coord: " + coordText.getAttribute("innerHTML"));
+		lat = findCoord(coordText.getAttribute("innerHTML"), true);
+		lon = findCoord(coordText.getAttribute("innerHTML"), false);
+		Utils.assertLatInRange("Sri Lanka Jump", lat, SriLankaLat, 5);
+		Utils.assertLonInRange("Sri Lanka Jump", lon, SriLankaLon, 5);
+
+		// Check +Antimeridian
+		searchButton.click();
+		Thread.sleep(1000);
+		coordField.sendKeys(Double.toString(PosDateLineLat) + ", " + Double.toString(PosDateLineLon));
+		Thread.sleep(1000);
+		submitButton.click();
 		
+		robot.mouseMove(351, 351);
+		lat = findCoord(coordText.getAttribute("innerHTML"), true);
+		lon = findCoord(coordText.getAttribute("innerHTML"), false);
+		Utils.assertLatInRange("+180 Longitude Jump", lat, PosDateLineLat, 5);
+		Utils.assertLonInRange("+180 Longitude Jump", lon, PosDateLineLon, 5);
+		
+		// Check -Antimeridian
+		searchButton.click();
+		Thread.sleep(1000);
+		coordField.sendKeys(Double.toString(NegDateLineLat) + ", " + Double.toString(NegDateLineLon));
+		Thread.sleep(5000);
+		submitButton.click();
+		
+		robot.mouseMove(352, 352);
+		lat = findCoord(coordText.getAttribute("innerHTML"), true);
+		lon = findCoord(coordText.getAttribute("innerHTML"), false);
+		Utils.assertLatInRange("-180 Longitude Jump", lat, NegDateLineLat, 5);
+		Utils.assertLonInRange("-180 Longitude Jump", lon, NegDateLineLon, 5);
+		
+		// Check North Pole
+		searchButton.click();
+		Thread.sleep(1000);
+		coordField.sendKeys(Double.toString(NorthPoleLat) + ", " + Double.toString(NorthPoleLon));
+		Thread.sleep(1000);
+		submitButton.click();
+		
+		robot.mouseMove(353, 353);
+		lat = findCoord(coordText.getAttribute("innerHTML"), true);
+		lon = findCoord(coordText.getAttribute("innerHTML"), false);
+		Utils.assertLatInRange("North Pole Jump", lat, NorthPoleLat, 5);
+		// No need to check longitude at poles.
+		
+		// Check South Pole
+		searchButton.click();
+		Thread.sleep(1000);
+		coordField.sendKeys(Double.toString(SouthPoleLat) + ", " + Double.toString(SouthPoleLon));
+		Thread.sleep(1000);
+		submitButton.click();
+		
+		robot.mouseMove(354, 354);
+		lat = findCoord(coordText.getAttribute("innerHTML"), true);
+		lon = findCoord(coordText.getAttribute("innerHTML"), false);
+		Utils.assertLatInRange("South Pole Jump", lat, SouthPoleLat, 5);
+		// No need to check longitude at poles.
 	}
 	
 	@After 
 	public void tearDown() throws Exception {
 		driver.quit();
+	}
+	
+	private double findCoord(String input, boolean isLat) {
+		Pattern p;
+		Matcher m;
+		int sign;
+		if (isLat) {
+			p = Pattern.compile("(\\d+)(?=°[^EW]*[NS])");
+			if (input.indexOf('N') >= 0) {
+				sign = 1;
+			} else {
+				sign = -1;
+			}
+		} else {
+			p = Pattern.compile("(\\d+)(?=°[^NS]*[EW])");
+			if (input.indexOf('E') >= 0) {
+				sign = 1;
+			} else {
+				sign = -1;
+			}
+		}
+		m = p.matcher(input);
+		assertTrue("Cursor coordinates should be visible on screen", m.find());
+		return sign*Double.parseDouble(m.group());
 	}
 }
--- ci/Selenium/src/test/java/bfui/test/Utils.java
+++ ci/Selenium/src/test/java/bfui/test/Utils.java
@@ -1,5 +1,6 @@
 package bfui.test;
 
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -25,5 +26,33 @@ public class Utils {
 	  
 	  static WebElement getFocusedField(WebDriver driver) {
 		  return driver.switchTo().activeElement();
+	  }
+	  
+	  static void assertLatInRange(double actual, double target, double range) {
+		  assertLatInRange("", actual, target, range);
+	  }
+	  
+	  static void assertLatInRange(String msg, double actual, double target, double range) {
+		  assertTrue("Latitude should be within [-90,90]", Math.abs(actual) <= 90);
+		  if (msg.isEmpty()) {
+			  msg = "Latitude should be within %f degrees of the target";
+		  } else {
+			  msg += ": Latitude should be within %f degrees of the target";
+		  }
+		  assertTrue(String.format(msg, range), Math.abs(actual - target) < range);
+	  }
+	  
+	  static void assertLonInRange(double actual, double target, double range) {
+		  assertLonInRange("", actual, target, range);
+	  }
+	  
+	  static void assertLonInRange(String msg, double actual, double target, double range) {
+		  assertTrue("Longitude should be within [-180,180]", Math.abs(actual) <= 180);
+		  if (msg.isEmpty()) {
+			  msg = "Longitude should be within %f degrees of the target";
+		  } else {
+			  msg += ": Longitude should be within %f degrees of the target";
+		  }
+		  assertTrue(String.format(msg, range), Math.abs(actual - target) < range || Math.abs(actual - target) - 180 < range);
 	  }
 }
