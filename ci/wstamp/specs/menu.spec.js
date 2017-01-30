569fb23f73eeab2dda7c7da0715ff5ffd8e976e2
Bickston <Luke.Bickston@rgi-corp.com>
2017-01-30 12:13:27 -0500

    moved functionality into beforeEacha and afterEach

--- ci/wstamp/specs/menu.spec.js
+++ ci/wstamp/specs/menu.spec.js
@@ -26,48 +26,46 @@ describe('The homepage menu', function() {
 
   beforeEach(function(){
       browser.sleep(1000);
+      homePage.menuButton.click();
+  })
+
+  afterEach(function(){
+     homePage.closeModalButton.click(); 
   })
 
   it('should open a profile modal', function() {  
-    homePage.menuButton.click()
     homePage.profileButton.click();
     expect(homePage.modalTitle.getText()).toEqual('Account Settings');
-    homePage.closeModalButton.click();
   });
 
-  it('should open a group modal', function() {  
-    homePage.menuButton.click()
+  it('should open a groups modal', function() {  
     homePage.groupsButton.click();
     expect(homePage.modalTitle.getText()).toEqual('Group Management');
-    homePage.closeModalButton.click();
+  });
+
+  it('should open a manage datasets modal', function() {  
+    homePage.manageDatasetsButton.click();
+    expect(homePage.modalTitle.getText()).toEqual('Dataset Management');
   });
 
   it('should open an offer feedback modal', function() {  
-    homePage.menuButton.click()
     homePage.offerFeedbackButton.click();
     expect(homePage.modalTitle.getText()).toEqual('Offer Feedback');
-    homePage.closeModalButton.click();
   });
 
    it('should open a my stamps modal', function() {  
-    homePage.menuButton.click()
     homePage.myStampsButton.click();
     expect(homePage.modal.getText()).toContain('My Stamps');
-    homePage.closeModalButton.click();
   });
 
    it('should open a save modal', function() {  
-    homePage.menuButton.click()
     homePage.saveButton.click();
     expect(homePage.modalTitle.getText()).toEqual('Save STAMP');
-    homePage.closeModalButton.click();
   });
 
   it('should open a save as modal', function() {  
-    homePage.menuButton.click()
     homePage.saveAsButton.click();
     expect(homePage.modalTitle.getText()).toEqual('Save STAMP');
-    homePage.closeModalButton.click();
   });
 
 
